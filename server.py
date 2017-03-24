9e394a788ac07f51c033a8b835dfb3f7786b1638
Scott Shuffler <scottshuffler@gmail>
2017-03-24 13:27:03 +0000

    encryption added to server

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import socket
-port = 60000                 
-s = socket.socket()          
-#host = socket.gethostname() 
-s.bind(('', port))           
-s.listen(5)                  
-print 'Waiting for client...'
+port = 60000                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+#host = socket.gethostname()     # Get local machine name
+s.bind(('', port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
+print 'Server listening....'
 
-conn, addr = s.accept()    
+conn, addr = s.accept()     # Establish connection with client.
 data_lock = True
 
+print 'Got connection from', addr
 data = conn.recv(1024)
 print('Server received', repr(data))
 while True:
